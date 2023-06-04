079cd83e0768a65280906a40bbad630213c31dff
Sejin Kang <ks320205@naver.com>
2023-06-05 03:24:52 +0900

    테스트 구현 (디자인,변수 미적용)

--- testpage/personal_info.js
+++ testpage/personal_info.js
@@ -1,20 +1,31 @@
-function startTest() {
+function startTestpage() {
     var selectBox = document.getElementById("jobSelect");
     var selectedValue = selectBox.value;
 
-    if (selectedValue === "1") {
-      window.location.href = "1.html";
-    } else if (selectedValue === "2") {
-      window.location.href = "2.html";
-    } else if (selectedValue === "3") {
-      window.location.href = "3.html";
-    } else if (selectedValue === "4") {
-      window.location.href = "4.html";
-    } else if (selectedValue === "5") {
-      window.location.href = "5.html";
-    } else if (selectedValue === "6") {
-      window.location.href = "6.html";
-    } else if (selectedValue === "7") {
-      window.location.href = "7.html";
+    /*페이지 연결하는 코드 */
+    // if (selectedValue === "1") {
+    //   window.location.href = "1.html";
+    // } else if (selectedValue === "2") {
+    //   window.location.href = "2.html";
+    // } else if (selectedValue === "3") {
+    //   window.location.href = "3.html";
+    // } else if (selectedValue === "4") {
+    //   window.location.href = "4.html";
+    // } else if (selectedValue === "5") {
+    //   window.location.href = "5.html";
+    // } else if (selectedValue === "6") {
+    //   window.location.href = "6.html";
+    // } else if (selectedValue === "7") {
+    //   window.location.href = "7.html";
+    // }
+
+    if (selectedJob) {
+      var script = document.createElement("script");
+      script.src = "./data" + selectedJob + ".js";
+      script.charset = "utf-8";
+      document.head.appendChild(script);
     }
+  
+    // testpage.html로 이동
+    location.href = "testpage.html";
   }
