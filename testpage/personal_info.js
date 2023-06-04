e9d808ce9342fd119d95c03c2e4bc90165b38e01
Sejin Kang <ks320205@naver.com>
2023-06-05 00:10:23 +0900

    테스트 이동

---
+++ testpage/personal_info.js
@@ -0,0 +1,20 @@
+function startTest() {
+    var selectBox = document.getElementById("jobSelect");
+    var selectedValue = selectBox.value;
+
+    if (selectedValue === "1") {
+      window.location.href = "1.html";
+    } else if (selectedValue === "2") {
+      window.location.href = "2.html";
+    } else if (selectedValue === "3") {
+      window.location.href = "3.html";
+    } else if (selectedValue === "4") {
+      window.location.href = "4.html";
+    } else if (selectedValue === "5") {
+      window.location.href = "5.html";
+    } else if (selectedValue === "6") {
+      window.location.href = "6.html";
+    } else if (selectedValue === "7") {
+      window.location.href = "7.html";
+    }
+  }
