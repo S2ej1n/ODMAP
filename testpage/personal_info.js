201a71833b6fdf8914d8005109b6d201777b2e6e
Sejin Kang <ks320205@naver.com>
2023-06-05 21:58:22 +0900

    testpage.html, personal_info.js 수정해야됩니다

--- testpage/personal_info.js
+++ testpage/personal_info.js
@@ -1,28 +1,30 @@
-function startTestpage() {
+var selectedJsFile
+
+function startTest() {
     var selectBox = document.getElementById("jobSelect");
     var selectedValue = selectBox.value;
 
-    console.log(selectedValue)
-
     // JavaScript 파일 목록
-    var jsFiles = {
-      "1": "data1.js",
-      "2": "data2.js",
-      "3": "data3.js",
-      "4": "data4.js",
-      "5": "data5.js",
-      "6": "data6.js",
-      "7": "data7.js",
-      "8": "data8.js",
-      "9": "data9.js",
-      "10": "data10.js"
-    };
+    // var jsFiles = {
+    //   "1": "data1.js",
+    //   "2": "data2.js",
+    //   "3": "data3.js",
+    //   "4": "data4.js",
+    //   "5": "data5.js",
+    //   "6": "data6.js",
+    //   "7": "data7.js",
+    //   "8": "data8.js",
+    //   "9": "data9.js",
+    //   "10": "data10.js"
+    // };
 
-    // 선택한 값에 해당하는 JavaScript 파일을 가져옴
-    var selectedJsFile = jsFiles[selectedValue];
+    if (selectedValue == ""){
+      window.alert("직업군을 선택해주세요");
+    }
+    else{
+      window.location.href = 'testpage.html';
+      // 선택한 값에 해당하는 JavaScript 파일을 가져옴
+      // selectedJsFile = jsFiles[selectedValue];
 
-    return selectedJsFile
-    // if (selectedJsFile) {
-    //   window.location.href = "test.html?jsfile=" + selectedJsFile;
-    // }
+    }
   }
